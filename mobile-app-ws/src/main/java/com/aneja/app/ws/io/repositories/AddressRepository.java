c09bfcedeaa44d30b7cfd181771c965979437fd0
User <User@DESKTOP-3RVN1SA>
2021-04-13 11:58:03 +0100

    Initial commit of Projects

---
+++ mobile-app-ws/src/main/java/com/aneja/app/ws/io/repositories/AddressRepository.java
@@ -0,0 +1,18 @@
+package com.aneja.app.ws.io.repositories;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.aneja.app.ws.io.entity.AddressEntity;
+import com.aneja.app.ws.io.entity.UserEntity;
+
+@Repository
+public interface AddressRepository extends CrudRepository<AddressEntity, Long> {
+
+	List<AddressEntity> findAllByUserDetails(UserEntity userEntity);
+	
+	AddressEntity findByAddressId(String addressId);
+
+}
